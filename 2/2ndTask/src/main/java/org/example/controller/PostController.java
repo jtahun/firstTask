e54d46d79a458c9bc8a0ea85e4b4d88cc8cf9d61
igor.zhuley <jtahun@bk.ru>
2026-01-11 22:27:39 +0500

    added getById i create in postController

--- 2/2ndTask/src/main/java/org/example/controller/PostController.java
+++ 2/2ndTask/src/main/java/org/example/controller/PostController.java
@@ -1,4 +1,28 @@
 package ru.igor.crud.controller;
 
 public class PostController {
+    private final PostRepository postRepository;
+    private final LabelRepository labelRepository;
+
+    public PostController(PostRepository postRepository, LabelRepository labelRepository) {
+        this.postRepository = postRepository;
+        this.labelRepository = labelRepository;
+    }
+
+    public Post create(String title, String content, List<Long> labelIds) {
+        validateNotBlank(title, "title");
+  
+        Post p = new Post();
+        p.setTitle(title);
+        p.setContent(content == null ? "" : content);
+        p.setLabelIds(labelIds);
+        p.setStatus(Status.ACTIVE);
+        return postRepository.save(p);
+    }
+
+    public Post getById(long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Post id=" + id + " не найден"));
+    }
+
 }

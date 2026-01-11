60813022add4bfdb75a44ead35c21a5322e33b32
igor.zhuley <jtahun@bk.ru>
2026-01-11 22:01:11 +0500

    realized create meth

--- 2/2ndTask/src/main/java/org/example/controller/WriterController.java
+++ 2/2ndTask/src/main/java/org/example/controller/WriterController.java
@@ -10,4 +10,27 @@ public class WriterController {
         this.postRepository = postRepository;
     }
 
+    public Writer create(String firstName, String lastName, List<Long> postIds) {
+        validateName(firstName, "firstName");
+        validateName(lastName, "lastName");
+
+        if (postIds != null) {
+            // проверяем, что посты существуют и ACTIVE
+            for (Long postId : postIds) {
+                var post = postRepository.findById(postId).orElseThrow(
+                        () -> new ValidationException("Post id=" + postId + " не найден"));
+                if (post.getStatus() == Status.DELETED) {
+                    throw new ValidationException("Post id=" + postId + " имеет статус DELETED");
+                }
+            }
+        }
+
+        Writer w = new Writer();
+        w.setFirstName(firstName);
+        w.setLastName(lastName);
+        w.setPostIds(postIds);
+        w.setStatus(Status.ACTIVE);
+        return writerRepository.save(w);
+    }
+
 }

15a1422524726ca6cd52391bea970b82b5cbeac1
igor.zhuley <jtahun@bk.ru>
2026-01-11 21:14:50 +0500

    completed actions with Post

--- 2/2ndTask/src/main/java/org/example/view/Post.java
+++ 2/2ndTask/src/main/java/org/example/view/Post.java
@@ -35,5 +35,30 @@ public class Post {
         this.labelIds = (labelIds == null) ? new ArrayList<>() : new ArrayList<>(labelIds);
     }
 
-   
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = (status == null) ? Status.ACTIVE : status; }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + (content == null ? "" : content.replace("\n","\\n")) + '\'' +
+                ", labelIds=" + labelIds +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

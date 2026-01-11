dae09b7837ff06c054c57723315f866ffca6257f
igor.zhuley <jtahun@bk.ru>
2026-01-11 21:00:18 +0500

    Label made string equals hash

--- 2/2ndTask/src/main/java/org/example/view/Label.java
+++ 2/2ndTask/src/main/java/org/example/view/Label.java
@@ -24,5 +24,25 @@ public class Label {
     public Status getStatus() { return status; }
     public void setStatus(Status status) { this.status = (status == null) ? Status.ACTIVE : status; }
 
-   
+    @Override
+    public String toString() {
+        return "Label{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return Objects.equals(id, label.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

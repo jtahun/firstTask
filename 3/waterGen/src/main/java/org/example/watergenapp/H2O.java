6506cdd0c456cbb0cfeb2a968fe71b9eea803d52
jtahun <jtahun@bk.ru>
2026-02-02 00:19:54 +0500

    Implement H2O class with semaphore, barrier and hydro oxy meths
    Added semaphores and a cyclic barrier to manage H2O molecule creation.

--- 3/waterGen/src/main/java/org/example/watergenapp/H2O.java
+++ 3/waterGen/src/main/java/org/example/watergenapp/H2O.java
@@ -1,4 +1,27 @@
 package org.example.watergenapp;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+
 public class H2O {
+  private final Semaphore hSemaphore = new Semaphore(2);
+  private final Semaphore oSemaphore = new Semaphore(1);
+
+  private final CyclicBarrier barrier = new CyclicBarrier(3, () -> {
+    hSemaphore.release(2);
+    oSemaphore.release(1);
+    });
+
+  public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+    oSemaphore.acquire();
+    releaseOxygen.run();
+    awaitBarrier();
+  }
+
+  public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+    hSemaphore.acquire();
+    releaseHydrogen.run();
+    awaitBarrier();
+  }
 }
